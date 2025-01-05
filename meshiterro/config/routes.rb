a01a9a20172989b3f4f9dd3069f45711dbca62af
KatoKoshi0209 <doradorayuji@gmail.com>
2025-01-05 06:32:32 +0000

    モデルの準備2

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
   get 'homes/about', to: 'homes#about', as: :about
+  resources :post_images, only: [:new, :index, :show]
   # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
