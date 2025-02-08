668bf5bc6088052e85acc141bc43c2d4875ca50d
KatoKoshi0209 <doradorayuji@gmail.com>
2025-02-08 01:23:35 +0000

    左側ページの作成

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
+    @user = current_user
   end
   
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
